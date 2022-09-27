70f28f03194cf783021c468d896ffedd69f5f52a
73X73-1LL1518L3 <107544841+73X73-1LL1518L3@users.noreply.github.com>
2022-09-27 12:03:02 +0200

    hey deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenu sur le site de Raphaël Gomes Caillard hey';
+  title = 'Bienvenu sur le site de Raphaël Gomes Caillard';
 }
