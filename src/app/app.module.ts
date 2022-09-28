520c89bc381d5596df479d8fc62c53d6b5153a19
73X73-1LL1518L3 <107544841+73X73-1LL1518L3@users.noreply.github.com>
2022-09-28 15:18:09 +0200

    creat raclette party

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { RaclettePartyComponent } from './raclette-party/raclette-party.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
-    UserProfileComponent
+    UserProfileComponent,
+    RaclettePartyComponent,
   ],
   imports: [
     BrowserModule,
